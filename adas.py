4635df3cfda294fc75d5e7aecd82f69d1178c1b4
helooo69778 <animerakesh28@gmail.com>
2025-07-26 10:40:22 +0530

    Add files via upload

---
+++ adas.py
@@ -0,0 +1,3 @@
+Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=[1,2,3,4,4,5,6,6,7,7]
